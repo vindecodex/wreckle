2969ad0c2b8a105841f80404b568aaa8517d8b20
vincent villaluna <villalunavincent@gmail.com>
2022-04-15 17:22:33 +0800

    Update naming and access modifier

--- selectionbox/selectionbox.go
+++ selectionbox/selectionbox.go
@@ -6,19 +6,19 @@ import (
 )
 
 type Model struct {
-	index    int
-	selected string
-	options  []string
+	Index    int
+	Selected string
+	Options  []string
 }
 
 func New() Model {
 	return Model{
-		index:    0,
-		selected: "",
-		options: []string{
-			"Red",
-			"Green",
-			"Blue",
+		Index:    0,
+		Selected: "",
+		Options: []string{
+			"red",
+			"green",
+			"blue",
 		},
 	}
 }
@@ -31,17 +31,17 @@ func (m Model) Update(msg tea.Msg) (Model, tea.Cmd) {
 		switch msg.String() {
 
 		case "up", "k":
-			if m.index > 0 {
-				m.index--
+			if m.Index > 0 {
+				m.Index--
 			}
 
 		case "down", "j":
-			if m.index < len(m.options)-1 {
-				m.index++
+			if m.Index < len(m.Options)-1 {
+				m.Index++
 			}
 
 		case "enter", " ":
-			m.selected = m.options[m.index]
+			m.Selected = m.Options[m.Index]
 		}
 	}
 
@@ -51,9 +51,9 @@ func (m Model) Update(msg tea.Msg) (Model, tea.Cmd) {
 func (m Model) View() string {
 	var options []string
 
-	for i, option := range m.options {
+	for i, option := range m.Options {
 
-		if m.index == i {
+		if m.Index == i {
 			options = append(options, lipgloss.NewStyle().Align(lipgloss.Center).Foreground(lipgloss.Color("#000000")).Background(lipgloss.Color("#ffffff")).Width(50).Render(option+"\n"))
 		} else {
 			options = append(options, lipgloss.NewStyle().Align(lipgloss.Center).Width(50).Render(option+"\n"))
