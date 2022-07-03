4809b08558dc6e64560973a6bc16ef8e3903c441
vincent villaluna <villalunavincent@gmail.com>
2022-07-03 11:46:06 +0800

    Rework: change model structure and flow

--- main.go
+++ main.go
@@ -3,177 +3,237 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
+	"github.com/vindecodex/wreckle/c"
 	"github.com/vindecodex/wreckle/inputbox"
-	"github.com/vindecodex/wreckle/selectionbox"
-	"github.com/vindecodex/wreckle/utils"
 	"golang.org/x/crypto/ssh/terminal"
 )
 
+const (
+	MAXCOL = 5
+	MAXROW = 6
+)
+
+type State interface {
+	onBackspace()
+	onEnter()
+	handleInput(msg string)
+	handleColor(msg string)
+	handlePick(msg string)
+	render() string
+}
+
+type InputState struct {
+	m *model
+}
+
+type ColorState struct {
+	m *model
+}
+
+type PickState struct {
+	m *model
+}
+
+func (h InputState) onBackspace() {
+	if h.m.col > 0 {
+		h.m.inputBoxes[h.m.row][h.m.col-1].Value = " "
+		h.m.col--
+	}
+}
+
+func (h InputState) onEnter() {
+	if h.m.col == MAXCOL && h.m.row < MAXROW-1 {
+		h.m.row++
+		h.m.col = 0
+		h.m.setState(h.m.colorState)
+	}
+}
+
+func (h InputState) handleInput(msg string) {
+	if h.m.col < MAXCOL {
+		h.m.inputBoxes[h.m.row][h.m.col].Value = strings.ToUpper(msg)
+		h.m.col++
+	}
+}
+
+func (h InputState) handleColor(msg string) {}
+
+func (h InputState) handlePick(msg string) {}
+
+func (h InputState) render() string {
+	var rowContainer []string
+	var gridContainer string
+	logo := logo()
+	for r := 0; r < MAXROW; r++ {
+		for c := 0; c < MAXCOL; c++ {
+			rowContainer = append(rowContainer, h.m.inputBoxes[r][c].View())
+		}
+		gridContainer += alignHorizontal(rowContainer...) + "\n"
+		rowContainer = nil
+	}
+
+	help := "Press [ctrl+c] to exit \n"
+	help += "Press [ctrl+r] to restart \n"
+
+	app := alignVertical(logo, gridContainer, help)
+	return lipgloss.Place(h.m.width, h.m.height, lipgloss.Center, lipgloss.Center, app)
+}
+
+func (h ColorState) onBackspace() {}
+
+func (h ColorState) onEnter() {}
+
+func (h ColorState) handleInput(msg string) {
+	if h.m.col < MAXCOL {
+		switch msg {
+		case "x":
+			h.m.inputBoxes[h.m.row-1][h.m.col].MetaTag = c.GREY
+			h.m.col++
+		case "g":
+			h.m.inputBoxes[h.m.row-1][h.m.col].MetaTag = c.GREEN
+			h.m.col++
+		case "y":
+			h.m.inputBoxes[h.m.row-1][h.m.col].MetaTag = c.YELLOW
+			h.m.col++
+
+		}
+	}
+
+}
+
+func (h ColorState) handleColor(msg string) {}
+
+func (h ColorState) handlePick(msg string) {}
+
+func (h ColorState) render() string {
+	var rowContainer []string
+	var gridContainer string
+	logo := logo()
+	guide := "x = gray, g = green, y = yellow"
+	for c := 0; c < MAXCOL; c++ {
+		rowContainer = append(rowContainer, h.m.inputBoxes[h.m.row-1][c].View())
+	}
+	gridContainer += lipgloss.NewStyle().MarginTop(2).Render(alignHorizontal(rowContainer...))
+
+	help := "Press [ctrl+c] to exit \n"
+	help += "Press [ctrl+r] to restart \n"
+
+	app := alignVertical(logo, guide, gridContainer, help)
+	return lipgloss.Place(h.m.width, h.m.height, lipgloss.Center, lipgloss.Center, app)
+}
+
+func (h PickState) onBackspace() {}
+
+func (h PickState) onEnter() {}
+
+func (h PickState) handleInput(msg string) {}
+
+func (h PickState) handleColor(msg string) {}
+
+func (h PickState) handlePick(msg string) {}
+
+func (h PickState) render() string {
+	return ""
+}
+
 type model struct {
-	red   string
-	blue  string
-	green string
+	inputBoxes [MAXROW][MAXCOL]inputbox.Model
 
-	status string
-
-	row int
 	col int
+	row int
 
 	width  int
 	height int
 
-	inputBoxes   [utils.ROW][utils.COL]inputbox.Model
-	selectionBox selectionbox.Model
+	state      State
+	inputState State
+	colorState State
+	pickState  State
 }
 
-func initialModel() model {
+func initialModel() *model {
 	width, height, _ := terminal.GetSize(0)
-	return model{
-		red:   "",
-		blue:  "",
-		green: "",
-
-		status: utils.SET_WORDS,
+	m := &model{
+		inputBoxes: generateInputBoxes(),
 
 		row: 0,
 		col: 0,
 
 		width:  width,
 		height: height,
-
-		inputBoxes:   generateInputBoxes(),
-		selectionBox: selectionbox.New(),
 	}
+
+	inputState := &InputState{m}
+	colorState := &ColorState{m}
+	pickState := &PickState{m}
+
+	m.state = inputState
+	m.inputState = inputState
+	m.colorState = colorState
+	m.pickState = pickState
+	return m
+}
+
+func (m *model) setState(state State) {
+	// clearing the terminal to fix rendering issue
+	fmt.Printf("\x1bc")
+	m.state = state
+}
+
+func generateInputBoxes() [MAXROW][MAXCOL]inputbox.Model {
+	inputBoxes := [MAXROW][MAXCOL]inputbox.Model{}
+	for r := 0; r < MAXROW; r++ {
+		for c := 0; c < MAXCOL; c++ {
+			inputBoxes[r][c] = inputbox.New()
+		}
+	}
+	return inputBoxes
 }
 
 func (m model) Init() tea.Cmd {
 	return nil
 }
 
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+func (m *model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
-
+	case tea.WindowSizeMsg:
+		m.width = msg.Width
+		m.height = msg.Height
+		return m, nil
 	case tea.KeyMsg:
-
 		switch msg.String() {
-
 		case "ctrl+c":
 			return m, tea.Quit
-
 		case "ctrl+r":
-			return m.reset(), nil
-
-		case "backspace":
-			if m.col > 0 {
-				m.inputBoxes[m.row][m.col-1].Value = 32
-				m.col--
-			}
-			return m, nil
-
-		case "up", "k", "down", "j":
-			m.selectionBox, _ = m.selectionBox.Update(msg)
-			return m, nil
-
+			return initialModel(), nil
 		case "enter":
-			if m.status == utils.SET_WORDS {
-				maxCol := len(m.inputBoxes[m.row]) - 1
-				if m.row < len(m.inputBoxes)-1 && m.inputBoxes[m.row][maxCol].Value != 32 {
-					m.row++
-					m.col = 0
-					m.status = utils.SET_COLOR
-				}
-			}
-			if m.status == utils.SET_COLOR {
-				m.inputBoxes[m.row][m.col].Color = m.selectionBox.Selected
-				m.inputBoxes[m.row][m.col] = m.inputBoxes[m.row][m.col]
-			}
+			m.state.onEnter()
 			return m, nil
-
+		case "backspace":
+			m.state.onBackspace()
+			return m, nil
 		default:
-			if m.status == utils.SET_WORDS {
-				if m.col < len(m.inputBoxes[m.row]) {
-					mdl, _ := m.inputBoxes[m.row][m.col].Update(msg)
-					m.inputBoxes[m.row][m.col] = mdl
-					m.col++
-				}
-			}
+			m.state.handleInput(msg.String())
 			return m, nil
 		}
-
 	}
-
 	return m, nil
 }
 
 func (m model) View() string {
-	var rowContainer []string
-	var grid string
-	var colorSelection string
-
-	logo := logo() + "\n\n"
-
-	if m.status == utils.SET_WORDS {
-
-		for row := 0; row < len(m.inputBoxes); row++ {
-			for col := 0; col < len(m.inputBoxes[m.row]); col++ {
-				rowContainer = append(rowContainer, m.inputBoxes[row][col].View())
-			}
-
-			grid += alignHorizontal(rowContainer...) + "\n"
-			rowContainer = nil
-		}
-	}
-
-	if m.status == utils.SET_COLOR {
-		for col := 0; col < len(m.inputBoxes[m.row]); col++ {
-			rowContainer = append(rowContainer, m.inputBoxes[m.row-1][col].View())
-		}
-		grid += alignHorizontal(rowContainer...) + "\n"
-		colorSelection = m.selectionBox.View()
-		rowContainer = nil
-	}
-
-	help := "\nPress ctrl + c to quit.\n"
-	help += "Press ctrl + r to restart.\n"
-
-	container := alignVertical(logo, grid, colorSelection, help)
-
-	return lipgloss.Place(m.width, m.height, lipgloss.Center, lipgloss.Center, container)
+	return m.state.render()
 }
 
-func (m model) reset() model {
-	width, height, _ := terminal.GetSize(0)
-	m.inputBoxes = generateInputBoxes()
-	m.row = 0
-	m.col = 0
-	m.red = ""
-	m.green = ""
-	m.blue = ""
-	m.status = utils.SET_WORDS
-	m.width = width
-	m.height = height
-	return m
-}
-
-func alignHorizontal(boxes ...string) string {
-	return lipgloss.JoinHorizontal(lipgloss.Bottom, boxes...)
+func alignHorizontal(components ...string) string {
+	return lipgloss.JoinHorizontal(lipgloss.Bottom, components...)
 }
 
 func alignVertical(components ...string) string {
 	return lipgloss.JoinVertical(lipgloss.Center, components...)
-}
-
-func generateInputBoxes() [utils.ROW][utils.COL]inputbox.Model {
-	ib := [utils.ROW][utils.COL]inputbox.Model{}
-	for row := 0; row < len(ib); row++ {
-		for col := 0; col < len(ib[row]); col++ {
-			ib[row][col] = inputbox.New()
-		}
-	}
-	return ib
 }
 
 func logo() string {
@@ -189,7 +249,7 @@ func logo() string {
 func main() {
 	p := tea.NewProgram(initialModel())
 	if err := p.Start(); err != nil {
-		fmt.Printf("Whoops! something went wrong", err)
+		fmt.Printf("Something went wrong: %v", err)
 		os.Exit(1)
 	}
 }
