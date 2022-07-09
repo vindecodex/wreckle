e572823a787a388aed13c1c5ec709ba19e5054bd
vincent villaluna <villalunavincent@gmail.com>
2022-07-09 11:43:05 +0800

    Update model and logic

--- inputbox/inputbox.go
+++ inputbox/inputbox.go
@@ -5,69 +5,49 @@ import (
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
-)
-
-var Colors = map[string]lipgloss.Color{
-	"white": lipgloss.Color("#ffffff"),
-	"red":   lipgloss.Color("#fc0574"),
-	"blue":  lipgloss.Color("#fc0574"),
-	"green": lipgloss.Color("#05fc91"),
-}
-
-const (
-	WHITE string = "white"
-	RED          = "red"
-	BLUE         = "blue"
-	GREEN        = "green"
+	"github.com/vindecodex/wreckle/c"
 )
 
 type Model struct {
-	Value byte
-	Color string
-}
-
-func initialModel() Model {
-	return Model{
-		Value: 32,
-		Color: WHITE,
-	}
-}
-
-func (m Model) Update(msg tea.Msg) (Model, tea.Cmd) {
-	switch msg := msg.(type) {
-
-	case tea.KeyMsg:
-
-		switch msg.String() {
-
-		default:
-			m.Value = []byte(strings.ToUpper(msg.String()))[0]
-			return m, nil
-		}
-	}
-
-	return m, nil
-}
-
-func (m Model) View() string {
-
-	return m.inputBox()
-}
-
-func (m Model) inputBox() string {
-	return lipgloss.NewStyle().
-		Bold(true).
-		PaddingLeft(2).
-		PaddingRight(2).
-		Foreground(Colors[m.Color]).
-		BorderStyle(lipgloss.NormalBorder()).
-		BorderForeground(Colors[m.Color]).
-		Render(string(m.Value))
+	Value   string
+	MetaTag string
 }
 
 func New() Model {
 	return Model{
-		Value: 32,
-		Color: "white",
+		Value:   " ",
+		MetaTag: c.WHITE,
 	}
 }
+
+func (m Model) Init() tea.Cmd {
+	return nil
+}
+
+func (m Model) Update(msg tea.Msg) (Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		default:
+			m.Value = strings.ToUpper(msg.String())
+			return m, nil
+		}
+	case Erase:
+		m.Value = " "
+		return m, nil
+	}
+	return m, nil
+}
+
+func (m Model) View() string {
+	return lipgloss.NewStyle().
+		PaddingLeft(2).
+		PaddingRight(2).
+		Bold(true).
+		Foreground(c.GetColor(m.MetaTag)).
+		BorderStyle(lipgloss.NormalBorder()).
+		BorderForeground(c.GetColor(m.MetaTag)).
+		Render(m.Value)
+}
+
+type Erase struct{}
